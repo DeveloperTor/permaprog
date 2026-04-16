0e9c94b29991af4b45b0f0bea9ddccba410087fd
Developer <developer.tor@gmail.com>
2026-04-16 22:06:19 +0200

    0.5.2

--- PermaProgCode/MainFile.cs
+++ PermaProgCode/MainFile.cs
@@ -23,7 +23,7 @@ public partial class MF : Node
         Log.WillLog(LogLevel.VeryDebug); // Not working :(
         // Have to update manually each release until I figure out an automatic way to get value from the JSON file
         var gameReleaseInfo = ReleaseInfoManager.Instance.ReleaseInfo;
-        var modVersion = Assembly.GetExecutingAssembly().GetName().Version = new Version(0, 5, 1);
+        var modVersion = Assembly.GetExecutingAssembly().GetName().Version = new Version(0, 5, 2);
         Log.Info($"Game version: {gameReleaseInfo?.Version}, branch: {gameReleaseInfo?.Branch}");
         Log.Info("Mod version: " + modVersion);
         ModConfigRegistry.Register(ModId, new PP());
