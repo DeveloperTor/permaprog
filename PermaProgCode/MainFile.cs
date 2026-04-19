2d53b242caef1ac8390e1bb30bf1b0029f3670ef
Developer <developer.tor@gmail.com>
2026-04-19 18:56:29 +0200

    Managed to add a top bar gold ui duplicate, no idea how to get/set value of it though

--- PermaProgCode/MainFile.cs
+++ PermaProgCode/MainFile.cs
@@ -6,6 +6,8 @@ using System.Reflection;
 using BaseLib.Config;
 using HarmonyLib;
 using Godot;
+using MegaCrit.Sts2.Core.Helpers;
+using MegaCrit.sts2.Core.Nodes.TopBar;
 
 namespace PermaProg.PermaProgCode;
 
@@ -14,8 +16,9 @@ public partial class MF : Node
 {
     public const string ModId = "PermaProg";
     public const string ResPath = $"res://{ModId}";
-
     public static Logger Log { get; } = new(ModId, LogType.Generic);
+
+    public static SceneTree? _tree;
 
     public static void Initialize()
     {
@@ -28,5 +31,62 @@ public partial class MF : Node
         Log.Info("Mod version: " + modVersion);
         ModConfigRegistry.Register(ModId, new PP());
         new Harmony(ModId).PatchAll();
+
+        _tree = Engine.GetMainLoop() as SceneTree;
+        if (_tree is not null)
+        {
+            _tree.NodeAdded += OnNodeAdded;
+        }
+    }
+
+    async private static void OnNodeAdded(Node node)
+    {
+        if (node.Name == "TopBar")
+        {
+            if (node is Control)
+            {
+                if (!node.IsNodeReady())
+                {
+                    await _tree.ToSignal(node, Node.SignalName.Ready);
+                }
+                Log.Info("TopBar:");
+                foreach (var VARIABLE in node.GetChildren())
+                {
+                    GD.Print(VARIABLE.Name);
+                }
+
+                var leftAligned = node.GetNode("LeftAlignedStuff");
+                Log.Info("LeftAlignedStuff:");
+                foreach (var VARIABLE in leftAligned.GetChildren())
+                {
+                    GD.Print(VARIABLE.Name);
+                }
+
+                var topBarGold = leftAligned.GetNode("TopBarGold");
+                Log.Info("TopBarGold:");
+                foreach (var VARIABLE in topBarGold.GetChildren())
+                {
+                    GD.Print(VARIABLE.Name);
+                }
+
+                NTopBarGold topBarGoldCopy = (NTopBarGold)leftAligned.GetNode("TopBarGold").Duplicate();
+                topBarGoldCopy.Name = "TopBarGoldCopy";
+                Log.Info("TopBarGoldCopy:");
+                foreach (var VARIABLE in topBarGoldCopy.GetChildren())
+                {
+                    GD.Print(VARIABLE.Name);
+                }
+
+                //topBarGoldCopy.Initialize();
+
+                // foreach (var VARIABLE in leftAligned.GetNode("TopBarGold").GetChildren())
+                // {
+                //     copy.AddChild(VARIABLE);
+                // }
+
+
+                leftAligned.AddChildSafely(topBarGoldCopy);
+            }
+        }
     }
 }
--- PermaProgCode/Patches/TopBarPatches.cs
+++ PermaProgCode/Patches/TopBarPatches.cs
@@ -12,56 +12,75 @@ namespace PermaProg.PermaProgCode.Patches;
 [HarmonyPatch]
 public class TopBarPatches
 {
-    [HarmonyPatch(typeof(NTopBar), "_Ready")]
-    [HarmonyPostfix]
-    public static void DoStuff1(ref NTopBar __instance)
-    {
-        GD.Print("In NTopBar ready");
-        foreach (var VARIABLE in __instance.GetChildren())
-        {
-            GD.Print(VARIABLE.Name);
-        }
-
-        var topBarGold = new NTopBarGold();
-        topBarGold.Name = "PermaProgtopBarGold";
-        __instance.AddChild(topBarGold);
-        //
-        // var goldMegaLabel = new MegaLabel();
-        // goldMegaLabel.Name = "GoldLabel";
-        // goldMegaLabel.AddThemeFontOverride(ThemeConstants.Label.font, font);
-        // var goldPopupMegaLabel = new MegaLabel();
-        // goldPopupMegaLabel.Name = "GoldPopup";
-        // goldPopupMegaLabel.AddThemeFontOverride(ThemeConstants.Label.font, font);
-        //
-        // topBarGold.AddChild(goldMegaLabel);
-        // topBarGold.AddChild(goldPopupMegaLabel);
-        //
-        // GD.Print("HERE");
-        // GD.Print(topBarGold.GetNode<MegaLabel>((NodePath) "%GoldLabel"));
-    }
-
-    private static Font font = new FontFile();
+    // [HarmonyPatch(typeof(NTopBar), "_Ready")]
+    // [HarmonyPostfix]
+    // public static void DoStuff1(ref NTopBar __instance)
+    // {
+    //     GD.Print(__instance.GetThemeFont(ThemeConstants.RichTextLabel.NormalFont));
+    //     _font = __instance.GetThemeFont(ThemeConstants.RichTextLabel.NormalFont);
+    //     GD.Print("In NTopBar ready");
+    //     foreach (var VARIABLE in __instance.GetChildren())
+    //     {
+    //         GD.Print(VARIABLE.Name);
+    //     }
+    //
+    //     var topBarGold = new NTopBarGold();
+    //     topBarGold.Name = "PermaProgTopBarGold";
+    //
+    //     var goldMegaLabel = new MegaLabel();
+    //     goldMegaLabel.Name = "GoldLabel";
+    //     goldMegaLabel.AddThemeFontOverride(ThemeConstants.RichTextLabel.NormalFont, _font);
+    //     var goldPopupMegaLabel = new MegaLabel();
+    //     goldPopupMegaLabel.Name = "GoldPopup";
+    //     goldPopupMegaLabel.Modulate = Colors.Transparent;
+    //     goldPopupMegaLabel.AddThemeFontOverride(ThemeConstants.RichTextLabel.NormalFont, _font);
+    //     topBarGold.AddChild(goldMegaLabel);
+    //     topBarGold.AddChild(goldPopupMegaLabel);
+    //
+    //
+    //     __instance.GetNode("LeftAlignedStuff").AddChild(topBarGold);
+    //     //__instance.AddChild(topBarGold);
+    //     //
+    //     // var goldMegaLabel = new MegaLabel();
+    //     // goldMegaLabel.Name = "GoldLabel";
+    //     // goldMegaLabel.AddThemeFontOverride(ThemeConstants.Label.font, font);
+    //     // var goldPopupMegaLabel = new MegaLabel();
+    //     // goldPopupMegaLabel.Name = "GoldPopup";
+    //     // goldPopupMegaLabel.AddThemeFontOverride(ThemeConstants.Label.font, font);
+    //     //
+    //     // topBarGold.AddChild(goldMegaLabel);
+    //     // topBarGold.AddChild(goldPopupMegaLabel);
+    //     //
+    //     // GD.Print("HERE");
+    //     // GD.Print(topBarGold.GetNode<MegaLabel>((NodePath) "%GoldLabel"));
+    // }
+    //
+     private static Font _font = new FontFile();
 
     [HarmonyPatch(typeof(NTopBarGold), "_Ready")]
     [HarmonyPrefix]
     public static bool DoStuff2(ref NTopBarGold __instance, ref MegaLabel ____goldLabel, ref MegaLabel ____goldPopupLabel)
     {
-        GD.Print(__instance.GetThemeFont(ThemeConstants.Label.font));
-        font = __instance.GetThemeFont(ThemeConstants.Label.font);
+        GD.Print(__instance.GetThemeFont(ThemeConstants.RichTextLabel.NormalFont));
+        _font = __instance.GetThemeFont(ThemeConstants.RichTextLabel.NormalFont);
 
-        if (__instance.Name != "PermaProgtopBarGold") return true;
-        GD.Print("In PermaProgtopBarGold ready");
+        if (__instance.Name != "TopBarGoldCopy") return true;
+        GD.Print("In TopBarGoldCopy _Ready");
 
 
-        var goldMegaLabel = new MegaLabel();
-        goldMegaLabel.Name = "GoldLabel";
-        goldMegaLabel.AddThemeFontOverride(ThemeConstants.Label.font, font);
-        var goldPopupMegaLabel = new MegaLabel();
-        goldPopupMegaLabel.Name = "GoldPopup";
-        goldPopupMegaLabel.Modulate = Colors.Transparent;
-        goldPopupMegaLabel.AddThemeFontOverride(ThemeConstants.Label.font, font);
-        ____goldLabel = goldMegaLabel;
-        ____goldPopupLabel = goldPopupMegaLabel;
+         ____goldLabel = __instance.GetNode<MegaLabel>("GoldLabel");
+         ____goldPopupLabel = __instance.GetParent().GetParent().GetNode<MegaLabel>("GoldPopup");
+         //__instance.Initialize(LocalContext.GetMe());
+
+        // var goldMegaLabel = new MegaLabel();
+        // goldMegaLabel.Name = "GoldLabel";
+        // goldMegaLabel.AddThemeFontOverride(ThemeConstants.RichTextLabel.NormalFont, _font);
+        // var goldPopupMegaLabel = new MegaLabel();
+        // goldPopupMegaLabel.Name = "GoldPopup";
+        // goldPopupMegaLabel.Modulate = Colors.Transparent;
+        // goldPopupMegaLabel.AddThemeFontOverride(ThemeConstants.RichTextLabel.NormalFont, _font);
+        // ____goldLabel = goldMegaLabel;
+        // ____goldPopupLabel = goldPopupMegaLabel;
         return false;
         //
         // __instance.AddChild(goldMegaLabel);
@@ -100,7 +119,16 @@ public class TopBarPatches
         foreach (var VARIABLE in __instance.GetChildren())
         {
             GD.Print(VARIABLE.Name);
+            GD.Print(VARIABLE.GetChildren());
+            GD.Print(" ");
         }
+
+        var test = (NTopBarGold)__instance.GetNode("LeftAlignedStuff").GetNode("TopBarGoldCopy");
+        GD.Print(test.Name);
+        var player = LocalContext.GetMe(runState);
+        // test._Ready();
+        // test.Initialize(player);
+
         // var topBarGold = new NTopBarGold();
         // topBarGold._Ready();
         // var player = LocalContext.GetMe(runState);
